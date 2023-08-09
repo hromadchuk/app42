92b9d092c737f4034928de5d10ed80bbf1ca412c
Pavel Gromadchuk <gromadchuk.pasha@gmail.com>
2023-08-10 01:56:10 +0300

    AnimatedMessages: finish code

--- src/methods/AnimatedMessages.tsx
+++ src/methods/AnimatedMessages.tsx
@@ -1,21 +1,70 @@
-import { useContext, useEffect, useState } from 'react';
-import { ActionIcon, Button, CopyButton, Input, Space, TextInput } from '@mantine/core';
-import { IconAt, IconCheck, IconCopy } from '@tabler/icons-react';
+import { useDisclosure } from '@mantine/hooks';
+import { JSX, useContext, useEffect, useState } from 'react';
+import {
+    Badge,
+    Button,
+    Card,
+    createStyles,
+    Group,
+    Image,
+    Input,
+    Modal,
+    Notification,
+    Space,
+    Text
+} from '@mantine/core';
+import { IconHeart, IconMail, TablerIconsProps } from '@tabler/icons-react';
 import { OwnerRow } from '../components/OwnerRow.tsx';
 import { Api } from 'telegram';
-import { CallAPI, sleep } from '../lib/helpers.tsx';
+import { CallAPI, getTextTime, sleep } from '../lib/helpers.tsx';
 
 import { AppContext } from '../components/AppContext.tsx';
 import { MethodContext } from '../components/MethodContext.tsx';
 
 import HeartAnimation from '../assets/animated_messages/heart.tsx';
+// @ts-ignore
+import HeartAnimationGif from '../assets/animated_messages/examples/heart.gif';
+
+interface IOption {
+    id: string;
+    icon: (props: TablerIconsProps) => JSX.Element;
+    withEndText?: boolean;
+    title: string;
+    frames: string[];
+}
+
+const FRAME_TIME = 100;
+
+const useStyles = createStyles((theme) => ({
+    card: {
+        cursor: 'pointer',
+
+        '&:hover': {
+            backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[0]
+        }
+    }
+}));
 
 export const AnimatedMessages = () => {
     const { user } = useContext(AppContext);
-    const { mt, needHideContent, setProgress } = useContext(MethodContext);
+    const { mt, needHideContent, setProgress, setFinishBlock, getProgress } = useContext(MethodContext);
+    const { classes } = useStyles();
+    const [opened, { open, close }] = useDisclosure(false);
 
     const [dialogsList, setDialogsList] = useState<Api.User[]>([]);
     const [selectedOwner, setSelectedOwner] = useState<Api.User | null>(null);
+    const [selectedOption, setSelectedOption] = useState<IOption | null>(null);
+    const [lastMessage, setLastMessage] = useState<string>('');
+
+    const options: IOption[] = [
+        {
+            id: 'heart',
+            icon: IconHeart,
+            withEndText: true,
+            title: mt('titles.heart'),
+            frames: HeartAnimation().split('\n\n')
+        }
+    ];
 
     useEffect(() => {
         getLastDialogs();
@@ -32,92 +81,184 @@ export const AnimatedMessages = () => {
         )) as Api.messages.Dialogs;
 
         const dialogs = result.dialogs
-            .filter((dialog) => dialog.peer instanceof Api.PeerUser || dialog.peer instanceof Api.PeerChat)
-            .map((dialog) => {
-                if (dialog.peer instanceof Api.PeerUser) {
-                    const peer = dialog.peer as Api.PeerUser;
-
-                    return result.users.find((findUser) => findUser.id.valueOf() === peer.userId.valueOf()) as Api.User;
+            .filter((dialog) => {
+                if (!(dialog.peer instanceof Api.PeerUser)) {
+                    return false;
                 }
 
-                const peer = dialog.peer as Api.PeerChat;
+                return dialog.peer.userId.valueOf() !== user?.id.valueOf();
+            })
+            .map((dialog) => {
+                const peer = dialog.peer as Api.PeerUser;
 
-                return result.chats.find((findChat) => findChat.id.valueOf() === peer.chatId.valueOf()) as Api.Chat;
+                return result.users.find((findUser) => findUser.id.valueOf() === peer.userId.valueOf()) as Api.User;
             });
-
-        console.log('dialogs', dialogs);
 
         setDialogsList(dialogs);
         setProgress(null);
     }
 
-    async function getOptions(owner: Api.User) {
-        console.log('owner', owner);
+    async function sendAnimation() {
+        setProgress({ text: mt('sending_message') });
 
-        // TODO logic for choice animate and add text
+        const owner = selectedOwner as Api.User;
+        const ownerId = owner.id.valueOf();
+        const lines = selectedOption?.frames.slice(0) as string[];
+        const firstLine = lines.shift() as string;
 
-        const lines = HeartAnimation().split('\n\n');
+        const messageId = await sendMessage(ownerId, firstLine);
 
-        console.log({lines});
+        if (!messageId) {
+            setFinishBlock({
+                text: mt('sending_message_error'),
+                state: 'error'
+            });
+            return;
+        }
 
-        // const sent = await CallAPI(
-        //     new Api.messages.SendMessage({
-        //         peer: owner.id.valueOf(),
-        //         message: 'test'
-        //     })
-        // );
+        setProgress({ text: mt('read_wait') });
 
-        // let newMessageId = 0;
-        //
-        // if (sent instanceof Api.Updates) {
-        //     newMessageId = (
-        //         sent.updates.find((update) => update instanceof Api.UpdateMessageID) as Api.UpdateMessageID
-        //     ).id.valueOf();
-        // }
-        //
-        // if (sent instanceof Api.UpdateShortSentMessage) {
-        //     newMessageId = sent.id.valueOf();
-        // }
-        //
-        // console.log({newMessageId});
+        await waitReadMessage(ownerId);
 
-        // const result = await CallAPI(
-        //     new Api.messages.GetMessagesViews({
-        //         peer: owner.id.valueOf(),
-        //         id: [811380, 811377]
-        //     })
-        // );
-        // console.log(result); // prints the result
+        if (lastMessage) {
+            lines.push(lastMessage);
+        }
 
-        // if (!newMessageId) {
-        //     return;
-        // }
-        //
-        // console.log({newMessageId});
-        //
-        // lines.push('Ты лалка!');
-        //
-        // for (const line of lines) {
-        //     const result = await window.TelegramClient.invoke(
-        //         new Api.messages.EditMessage({
-        //             peer: owner.id.valueOf(),
-        //             id: newMessageId,
-        //             message: line
-        //         })
-        //     );
-        //     console.log(result); // prints the result
-        //
-        //     await sleep(100);
-        // }
+        setProgress({ text: mt('loading_animation'), total: lines.length });
+
+        for (const line of lines) {
+            await CallAPI(
+                new Api.messages.EditMessage({
+                    peer: ownerId,
+                    id: messageId,
+                    message: line
+                })
+            );
+
+            const currentProgress = getProgress();
+            setProgress({ ...currentProgress, count: (currentProgress.count || 0) + 1 });
+
+            await sleep(FRAME_TIME);
+        }
+
+        setFinishBlock({});
+    }
+
+    async function sendMessage(ownerId: number, message: string): Promise<number> {
+        let newMessageId = 0;
+
+        const sent = await CallAPI(
+            new Api.messages.SendMessage({
+                peer: ownerId,
+                message
+            })
+        );
+
+        if (sent instanceof Api.Updates) {
+            newMessageId = (
+                sent.updates.find((update) => update instanceof Api.UpdateMessageID) as Api.UpdateMessageID
+            ).id.valueOf();
+        }
+
+        if (sent instanceof Api.UpdateShortSentMessage) {
+            newMessageId = sent.id.valueOf();
+        }
+
+        return newMessageId;
+    }
+
+    async function waitReadMessage(ownerId: number) {
+        let work = true;
+
+        while (work) {
+            const result = (await CallAPI(
+                new Api.messages.GetDialogs({
+                    offsetPeer: ownerId,
+                    limit: 100
+                })
+            )) as Api.messages.Dialogs;
+
+            const find = result.dialogs.find((dialog) => {
+                if (!(dialog.peer instanceof Api.PeerUser)) {
+                    return false;
+                }
+
+                return dialog.peer.userId.valueOf() === ownerId;
+            }) as Api.Dialog;
+
+            if (find.readOutboxMaxId === find.topMessage) {
+                work = false;
+            }
+
+            await sleep(1500);
+        }
+    }
+
+    function showModal(option: IOption) {
+        setSelectedOption(option);
+        open();
+    }
+
+    function OptionRow(option: IOption, key: number) {
+        const seconds = Math.round(option.frames.length / (1000 / FRAME_TIME));
+
+        return (
+            <Card withBorder radius="md" key={key} className={classes.card} onClick={() => showModal(option)}>
+                <Group position="apart">
+                    <div>
+                        <Text fw={500}>{option.title}</Text>
+                        {option.withEndText && (
+                            <Text fz="xs" c="dimmed">
+                                {mt('has_message_after_animation')}
+                            </Text>
+                        )}
+                    </div>
+                    <Badge size="xs">~{getTextTime(seconds)}</Badge>
+                </Group>
+            </Card>
+        );
     }
 
     if (needHideContent()) return null;
 
+    if (selectedOwner) {
+        return (
+            <>
+                <Modal opened={opened} onClose={close} title={selectedOption?.title}>
+                    <Image maw={240} mx="auto" mb="xs" radius="md" src={HeartAnimationGif} alt="Random image" />
+
+                    {selectedOption?.withEndText && (
+                        <Input
+                            icon={<IconMail size="1rem" />}
+                            placeholder={mt('message_after_animation')}
+                            onChange={(event) => setLastMessage(event.target.value)}
+                        />
+                    )}
+
+                    <Button fullWidth variant="outline" mt="xs" onClick={sendAnimation}>
+                        {mt('send_button')}
+                    </Button>
+                </Modal>
+
+                <Notification withCloseButton={false} mb="xs" color="gray">
+                    {mt('few_animation')}
+                </Notification>
+
+                <OwnerRow owner={selectedOwner} withoutLink={true} />
+                <Space h={20} />
+                {options.map(OptionRow)}
+            </>
+        );
+    }
+
     if (dialogsList.length) {
         return (
             <>
+                <Notification withCloseButton={false} mb="xs" color="gray">
+                    {mt('message_recipient')}
+                </Notification>
                 {dialogsList.map((dialog, key) => (
-                    <OwnerRow key={key} owner={dialog} callback={() => getOptions(dialog)} />
+                    <OwnerRow key={key} owner={dialog} callback={() => setSelectedOwner(dialog)} />
                 ))}
             </>
         );
