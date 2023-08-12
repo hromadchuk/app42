429cd17c63d67eddfee47a4af714f0564951267e
Pavel Gromadchuk <gromadchuk.pasha@gmail.com>
2023-08-12 23:59:56 +0300

    ClearChannelFollowers: state code

--- src/methods/ClearChannelFollowers.tsx
+++ src/methods/ClearChannelFollowers.tsx
@@ -2,6 +2,7 @@ import { useContext, useEffect, useState } from 'react';
 import { ActionIcon, Button, CopyButton, Input, Space, TextInput } from '@mantine/core';
 import { IconAt, IconCheck, IconCopy } from '@tabler/icons-react';
 import { Api } from 'telegram';
+import { ChannelParticipantsRecent } from 'telegram/tl/api';
 import { CallAPI, declineAndFormat } from '../lib/helpers.tsx';
 
 import { MethodContext } from '../components/MethodContext.tsx';
@@ -30,20 +31,39 @@ export const ClearChannelFollowers = () => {
     async function selectChannel(channel: Api.Channel) {
         console.log('channel', channel);
 
-        setProgress({ text: mt('loading_members'), total: channel.participantsCount as number });
+        // setProgress({ text: mt('loading_members'), total: channel.participantsCount as number });
+
+        const users = await CallAPI(
+            new Api.channels.GetParticipants({
+                channel,
+                filter: new Api.ChannelParticipantsRecent(),
+                offset: 100,
+                limit: 200,
+            })
+        );
     }
 
     if (needHideContent()) return null;
 
     if (channels) {
-        return channels.map((channel, key) => (
-            <OwnerRow
-                key={key}
-                owner={channel}
-                description={declineAndFormat(Number(channel.participantsCount), md('decline_members'))}
-                callback={() => selectChannel(channel)}
-            />
-        ));
+        return channels.map((channel, key) => {
+            const hasRights = Boolean(channel.adminRights?.banUsers);
+            const description = [declineAndFormat(Number(channel.participantsCount), md('decline_members'))];
+
+            if (!hasRights) {
+                description.push(mt('no_rights'));
+            }
+
+            return (
+                <OwnerRow
+                    key={key}
+                    owner={channel}
+                    disabled={!hasRights}
+                    description={description.join(', ')}
+                    callback={() => selectChannel(channel)}
+                />
+            );
+        });
     }
 
     return (
