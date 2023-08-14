74220e4ca960e3ffa81a2f2f7a1f77995fe388b0
Pavel Gromadchuk <gromadchuk.pasha@gmail.com>
2023-08-14 23:14:32 +0300

    Add hook for clear old localStorage

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,13 +1,17 @@
 interface IConstants {
     API_ID: number;
     API_HASH: string;
+    KIT_42_CACHE_VERSION: string;
     CACHE_PREFIX: string;
     SESSION_KEY: string;
 }
 
+const cacheVersion = 'V2';
+
 export const Constants: IConstants = {
     API_ID: 21504958,
     API_HASH: 'd4384e436536198944078fafd63aa051',
-    CACHE_PREFIX: 'kit42Cache',
-    SESSION_KEY: 'kit42SessionV2'
+    KIT_42_CACHE_VERSION: cacheVersion,
+    CACHE_PREFIX: 'kit42Cache' + cacheVersion,
+    SESSION_KEY: 'kit42Session' + cacheVersion
 };
