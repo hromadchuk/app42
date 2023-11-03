f9dd0ab6056ee03c74c9edc3ebcf4ee2adf54188
Kirill Malygin <[malygin.k@roistat.com](mailto:malygin.k@roistat.com)>
2023-11-04 02:28:52 +0300

    Add modal with common chats.

--- src/methods/CommonChatsTop.tsx
+++ src/methods/CommonChatsTop.tsx
@@ -1,18 +1,27 @@
 import { JSX, useContext, useEffect, useState } from 'react';
-import { Container } from '@mantine/core';
+import { Container, Modal } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
 import { Api } from 'telegram';
-import { CallAPI } from '../lib/helpers.tsx';
 
+import { CallAPI } from '../lib/helpers.tsx';
 import { MethodContext } from '../components/MethodContext.tsx';
 import { OwnerRow } from '../components/OwnerRow.tsx';
 
-interface IUserData {
-    user: Api.User;
-    common_chats_count: Api.int;
+interface ICommonChats {
+    chats: Api.TypeChat[];
+    count: Api.int;
 }
 
-export const CommonChatsTop = () => {
+interface IUserData {
+    user: Api.User;
+    common_chats: ICommonChats;
+}
+
+export const CommonChatsTop = (): JSX.Element | null => {
     const { mt, needHideContent, setProgress, setFinishBlock } = useContext(MethodContext);
+
+    const [isModalOpened, { open, close }] = useDisclosure(false);
+    const [modalChats, setModalChats] = useState<Api.TypeChat[]>([]);
 
     const [usersData, setUsersData] = useState<IUserData[]>([]);
 
@@ -20,7 +29,7 @@ export const CommonChatsTop = () => {
         getContacts();
     }, []);
 
-    async function getContacts() {
+    async function getContacts(): Promise<void> {
         setProgress({});
 
         const result = (await CallAPI(new Api.contacts.GetContacts({}))) as Api.contacts.Contacts;
@@ -31,33 +40,44 @@ export const CommonChatsTop = () => {
             return;
         }
 
-        const users = (result.users as Api.User[]).filter((user) => !user.self);
+        const users: Api.User[] = (result.users as Api.User[]).filter((user) => !user.self);
 
-        const usersWithCommonChatsCount = [];
+        const usersWithCommonChats: IUserData[] = [];
 
         for (const user of users) {
             setProgress({ count: users.indexOf(user), total: result.users.length });
 
-            const commonChatsCount = await getContactCommonChatsCount(user);
-            usersWithCommonChatsCount.push({ user, common_chats_count: commonChatsCount });
+            const commonChats: Api.TypeChat[] = (await getContactCommonChats(user)) as Api.TypeChat[];
+            usersWithCommonChats.push({
+                user,
+                common_chats: {
+                    chats: commonChats,
+                    count: commonChats.length
+                }
+            });
         }
 
-        usersWithCommonChatsCount.sort(
-            (userDataA, userDataB) => userDataB.common_chats_count - userDataA.common_chats_count
+        usersWithCommonChats.sort(
+            (userDataA, userDataB) => userDataB.common_chats.count - userDataA.common_chats.count
         );
 
-        setUsersData(usersWithCommonChatsCount);
+        setUsersData(usersWithCommonChats);
         setProgress(null);
     }
 
-    async function getContactCommonChatsCount(user: Api.User): Promise<Api.int> {
+    async function getContactCommonChats(user: Api.User): Promise<Api.TypeChat[]> {
         const result = (await CallAPI(
             new Api.messages.GetCommonChats({
                 userId: user.id
             })
         )) as Api.messages.Chats;
 
-        return result.chats.length;
+        return result.chats;
+    }
+
+    function openModal(chats: Api.TypeChat[]): void {
+        setModalChats(chats);
+        open();
     }
 
     function SectionBlock(): JSX.Element | null {
@@ -70,8 +90,9 @@ export const CommonChatsTop = () => {
                 {usersData.map((owner, key) => (
                     <OwnerRow
                         key={key}
+                        callback={owner.common_chats.count > 0 ? () => openModal(owner.common_chats.chats) : undefined}
                         owner={owner.user}
-                        description={`${mt('chats_count')}: ${owner.common_chats_count}`}
+                        description={`${mt('chats_count')}: ${owner.common_chats.count}`}
                     />
                 ))}
             </Container>
@@ -80,7 +101,23 @@ export const CommonChatsTop = () => {
 
     if (needHideContent()) return null;
 
-    return <SectionBlock />;
+    return (
+        <>
+            <Modal opened={isModalOpened} onClose={close} title={mt('chats_count')}>
+                {modalChats.map((chat, key) => (
+                    <OwnerRow
+                        key={key}
+                        owner={chat}
+                        description={`${mt('members_count')}: ${
+                            chat instanceof Api.Channel || chat instanceof Api.Chat ? chat.participantsCount : 0
+                        }`}
+                    />
+                ))}
+            </Modal>
+
+            <SectionBlock />
+        </>
+    );
 };
 
 export default CommonChatsTop;
