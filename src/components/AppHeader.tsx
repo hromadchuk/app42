ddb42458a89109bede6cb541bdc749dd98cfaebc
Pavel Gromadchuk <gromadchuk.pasha@gmail.com>
2023-11-04 03:24:51 +0300

    Add lottie logo and fix dialogs duplicate

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,13 +1,19 @@
 import { ActionIcon, AppShell, Center, Group, UnstyledButton } from '@mantine/core';
 import { IconArrowLeft } from '@tabler/icons-react';
-import { useEffect } from 'react';
+import { useContext, useEffect, useRef } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { Api } from 'telegram';
 import ReactGA from 'react-ga4';
+import Lottie, { LottieRefCurrentProps } from 'lottie-react';
 import { isDev } from '../lib/helpers.tsx';
 
-import Logo from './Logo.tsx';
 import { OwnerAvatar } from './OwnerAvatar.tsx';
+import LogoLottie from '../assets/logo_lottie.json';
+
+import { AppContext } from './AppContext.tsx';
+
+// @ts-ignore
+import classes from '../styles/AppHeader.module.css';
 
 interface IAppHeader {
     user: null | Api.User;
@@ -18,6 +24,9 @@ const excludeBackButton = ['/', '/menu'];
 export function AppHeader({ user }: IAppHeader) {
     const location = useLocation();
     const navigate = useNavigate();
+    const { isLoading } = useContext(AppContext);
+
+    const logoRef = useRef<LottieRefCurrentProps>(null);
 
     useEffect(() => {
         if (!isDev) {
@@ -31,13 +40,32 @@ export function AppHeader({ user }: IAppHeader) {
         }
     }, [location]);
 
+    useEffect(() => {
+        if (isLoading) {
+            logoRef.current?.play();
+        } else {
+            logoRef.current?.stop();
+        }
+    }, [isLoading]);
+
     const LeftSide = () => {
         if (excludeBackButton.includes(location.pathname)) {
             return null;
         }
 
         return (
-            <ActionIcon variant="transparent" size="xl" onClick={() => navigate('/menu')}>
+            <ActionIcon
+                variant="transparent"
+                size="xl"
+                onClick={() => {
+                    navigate('/menu');
+
+                    if (isLoading) {
+                        // need for stop all requests
+                        window.location.reload();
+                    }
+                }}
+            >
                 <IconArrowLeft />
             </ActionIcon>
         );
@@ -61,7 +89,7 @@ export function AppHeader({ user }: IAppHeader) {
                 <LeftSide />
 
                 <Center mx="auto">
-                    <Logo size={28} />
+                    <Lottie lottieRef={logoRef} animationData={LogoLottie} className={classes.logo} autoplay={false} />
                 </Center>
 
                 <RightSide />
