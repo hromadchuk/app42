6244cb53a856a1464128908867d89314e72a5ad1
Pavel Gromadchuk <gromadchuk.pasha@gmail.com>
2023-11-05 16:48:29 +0300

    Some changes for CommonChatsTop

--- src/methods/CommonChatsTop.tsx
+++ src/methods/CommonChatsTop.tsx
@@ -1,9 +1,9 @@
 import { JSX, useContext, useEffect, useState } from 'react';
-import { Container, Modal } from '@mantine/core';
+import { Divider, Modal } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import { Api } from 'telegram';
 
-import { CallAPI } from '../lib/helpers.tsx';
+import { CallAPI, declineAndFormat } from '../lib/helpers.tsx';
 import { MethodContext } from '../components/MethodContext.tsx';
 import { OwnerRow } from '../components/OwnerRow.tsx';
 
@@ -14,15 +14,15 @@ interface ICommonChats {
 
 interface IUserData {
     user: Api.User;
-    common_chats: ICommonChats;
+    commonChats: ICommonChats;
 }
 
 export const CommonChatsTop = (): JSX.Element | null => {
-    const { mt, needHideContent, setProgress, setFinishBlock } = useContext(MethodContext);
+    const { mt, md, needHideContent, setProgress, setFinishBlock } = useContext(MethodContext);
 
     const [isModalOpened, { open, close }] = useDisclosure(false);
     const [modalChats, setModalChats] = useState<Api.TypeChat[]>([]);
-
+    const [modalContact, setModalContact] = useState<Api.User | null>(null);
     const [usersData, setUsersData] = useState<IUserData[]>([]);
 
     useEffect(() => {
@@ -41,25 +41,30 @@ export const CommonChatsTop = (): JSX.Element | null => {
         }
 
         const users: Api.User[] = (result.users as Api.User[]).filter((user) => !user.self);
-
         const usersWithCommonChats: IUserData[] = [];
 
         for (const user of users) {
             setProgress({ count: users.indexOf(user), total: result.users.length });
 
             const commonChats: Api.TypeChat[] = (await getContactCommonChats(user)) as Api.TypeChat[];
-            usersWithCommonChats.push({
-                user,
-                common_chats: {
-                    chats: commonChats,
-                    count: commonChats.length
-                }
-            });
+            if (commonChats.length) {
+                usersWithCommonChats.push({
+                    user,
+                    commonChats: {
+                        chats: commonChats,
+                        count: commonChats.length
+                    }
+                });
+            }
         }
 
-        usersWithCommonChats.sort(
-            (userDataA, userDataB) => userDataB.common_chats.count - userDataA.common_chats.count
-        );
+        if (!usersWithCommonChats.length) {
+            setProgress(null);
+            setFinishBlock({ state: 'error', text: mt('no_common_chats') });
+            return;
+        }
+
+        usersWithCommonChats.sort((userDataA, userDataB) => userDataB.commonChats.count - userDataA.commonChats.count);
 
         setUsersData(usersWithCommonChats);
         setProgress(null);
@@ -75,44 +80,44 @@ export const CommonChatsTop = (): JSX.Element | null => {
         return result.chats;
     }
 
-    function openModal(chats: Api.TypeChat[]): void {
-        setModalChats(chats);
+    function openModal(owner: IUserData): void {
+        setModalContact(owner.user);
+        setModalChats(owner.commonChats.chats);
         open();
     }
 
-    function SectionBlock(): JSX.Element | null {
-        if (!usersData.length) {
-            return null;
-        }
-
-        return (
-            <Container px={0}>
-                {usersData.map((owner, key) => (
-                    <OwnerRow
-                        key={key}
-                        callback={owner.common_chats.count > 0 ? () => openModal(owner.common_chats.chats) : undefined}
-                        owner={owner.user}
-                        description={`${mt('chats_count')}: ${owner.common_chats.count}`}
-                    />
-                ))}
-            </Container>
-        );
+    function SectionBlock() {
+        return usersData.map((owner, key) => (
+            <OwnerRow
+                key={key}
+                callback={() => openModal(owner)}
+                owner={owner.user}
+                description={declineAndFormat(owner.commonChats.count, md('decline.chats'))}
+            />
+        ));
     }
 
     if (needHideContent()) return null;
 
     return (
         <>
-            <Modal opened={isModalOpened} onClose={close} title={mt('chats_count')}>
-                {modalChats.map((chat, key) => (
-                    <OwnerRow
-                        key={key}
-                        owner={chat}
-                        description={`${mt('members_count')}: ${
-                            chat instanceof Api.Channel || chat instanceof Api.Chat ? chat.participantsCount : 0
-                        }`}
-                    />
-                ))}
+            <Modal opened={isModalOpened} onClose={close} title={mt('modal_title')}>
+                <OwnerRow owner={modalContact} />
+                <Divider my="sm" />
+
+                {modalChats.map((chat, key) => {
+                    const membersCount = Number(
+                        (chat instanceof Api.Channel || chat instanceof Api.Chat) && chat.participantsCount
+                    );
+
+                    return (
+                        <OwnerRow
+                            key={key}
+                            owner={chat}
+                            description={declineAndFormat(membersCount, md('decline.members'))}
+                        />
+                    );
+                })}
             </Modal>
 
             <SectionBlock />
