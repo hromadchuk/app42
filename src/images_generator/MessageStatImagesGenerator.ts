cd0ca1fbf47c5596e39d5b27dd464a69749abd30
Pavel Gromadchuk <gromadchuk.pasha@gmail.com>
2024-03-06 01:18:52 +0300

    Some sharing changes

--- src/images_generator/MessageStatImagesGenerator.ts
+++ src/images_generator/MessageStatImagesGenerator.ts
@@ -1,22 +1,22 @@
 import { BaseImagesGenerator, IImagesGeneratorOptions, IImagesGeneratorResponse } from './BaseImagesGenerator.ts';
 
+interface IMessagesStatImagesUserOption {
+    name: string;
+    avatar?: string;
+    description: string;
+}
+
 export interface IMessagesStatImagesOptions extends IImagesGeneratorOptions {
-    users: {
-        name: string;
-        avatar?: string;
-        description: string;
-    }[];
+    users: IMessagesStatImagesUserOption[];
 }
 
 export class MessageStatImagesGenerator extends BaseImagesGenerator {
     async generate(options: IMessagesStatImagesOptions): Promise<IImagesGeneratorResponse> {
+        options.storyImage = false;
+
         await super.prepareProcess(options);
 
-        await this.drawMockImage('messages_stat');
-
-        if (options.storyImage) {
-            await this.drawStoryImage(options);
-        }
+        // await this.drawMockImage('messages_stat');
 
         if (options.messageImage) {
             await this.drawMessageImage(options);
@@ -25,81 +25,89 @@ export class MessageStatImagesGenerator extends BaseImagesGenerator {
         return this.getResult();
     }
 
-    async drawStoryImage(data: IMessagesStatImagesOptions) {
-        const { title, description, bottomText, users } = data;
-
-        // this.storyContext.font = `136px ${this.fontFamilyBold}`;
-        // this.storyContext.fillText(title, this.storyCanvasCenter, 940);
-        //
-        // if (subTitle) {
-        //     this.storyContext.globalAlpha = 0.7;
-        //     this.storyContext.font = `60px ${this.fontFamilyBold}`;
-        //     this.storyContext.fillText(subTitle, this.storyCanvasCenter, 1040);
-        // }
-        //
-        // this.storyContext.globalAlpha = 0.5;
-        //
-        // const descriptionYPosition = subTitle ? 1120 : 1034;
-        // this.storyContext.font = `40px ${this.fontFamilyBold}`;
-        // this.storyContext.fillText(description, this.storyCanvasCenter, descriptionYPosition);
-        //
-        // const bottomTextYPosition = 1390;
-        // this.storyContext.font = `28px ${this.fontFamilyBold}`;
-        // this.storyContext.fillText(bottomText, this.storyCanvasCenter, bottomTextYPosition);
-        // this.storyContext.fillText(bottomDateText, this.storyCanvasCenter, bottomTextYPosition + 40);
-        //
-        // this.storyContext.globalAlpha = 1;
-        //
-        // await this.drawAvatar(this.storyContext, data.avatar, 70, this.storyCanvasCenter - 140, 420, data.bottomText);
-        //
-        // this.storyContext.drawImage(await this.getImage('./items/reg_date/story_confiti.png'), 126, 190);
-        // this.storyContext.drawImage(
-        //     await this.getImage('./items/reg_date/story_circle.png'),
-        //     this.storyCanvasCenter - 145,
-        //     415
-        // );
-    }
-
     async drawMessageImage(data: IMessagesStatImagesOptions) {
         const { title, description, bottomText, users } = data;
 
-        // this.messageContext.textAlign = 'center';
-        // this.messageContext.font = `136px ${this.fontFamilyBold}`;
-        // this.messageContext.fillStyle = this.mainColor;
-        // this.messageContext.fillText(title, this.messageCanvasCenter, 680);
-        //
-        // if (subTitle) {
-        //     this.messageContext.globalAlpha = 0.7;
-        //     this.messageContext.font = `60px ${this.fontFamilyBold}`;
-        //     this.messageContext.fillText(subTitle, this.messageCanvasCenter, 760);
-        // }
-        //
-        // this.messageContext.globalAlpha = 0.5;
-        //
-        // const descriptionYPosition = subTitle ? 810 : 780;
-        // this.messageContext.font = `40px ${this.fontFamilyBold}`;
-        // this.messageContext.fillText(description, this.messageCanvasCenter, descriptionYPosition);
-        //
-        // this.messageContext.textAlign = 'right';
-        // this.messageContext.font = `20px ${this.fontFamilyBold}`;
-        // this.messageContext.fillText(`${bottomText} ${bottomDateText}`, this.messageCanvas.width - 50, 960);
-        //
-        // this.messageContext.globalAlpha = 1;
-        //
-        // await this.drawAvatar(
-        //     this.messageContext,
-        //     data.avatar,
-        //     70,
-        //     this.messageCanvasCenter - 140,
-        //     214,
-        //     data.bottomText
-        // );
-        //
-        // this.messageContext.drawImage(await this.getImage('./items/reg_date/message_confiti.png'), 150, 120);
-        // this.messageContext.drawImage(
-        //     await this.getImage('./items/reg_date/message_circle.png'),
-        //     this.messageCanvasCenter - 146,
-        //     210
-        // );
+        // title
+        this.messageContext.font = `67px ${this.fontFamilyBold}`;
+        this.messageContext.fillStyle = this.mainColor;
+        this.messageContext.fillText(title, 80, 150);
+
+        // description and bottom text
+        this.messageContext.globalAlpha = 0.5;
+        this.messageContext.font = `20px ${this.fontFamilyBold}`;
+        this.messageContext.fillText(description, 80, 204);
+
+        this.messageContext.textAlign = 'right';
+        this.messageContext.font = `20px ${this.fontFamilyBold}`;
+        this.messageContext.fillText(bottomText, this.messageCanvas.width - 80, 960);
+        this.messageContext.globalAlpha = 1;
+
+        const yTop3 = users.length === 3 ? 430 : 312;
+        await this.drawTopUsers(367, yTop3, users);
+
+        const otherUsers = users.slice(3, 8);
+        const center = this.messageCanvasCenter;
+        let moreCenter = center - (otherUsers.length - 1) * 106.5;
+        let userNumber = 4;
+
+        for (const user of otherUsers) {
+            await this.drawAvatar(
+                this.messageContext,
+                user.avatar || null,
+                28,
+                moreCenter - 28 * 2 + 6,
+                657,
+                user.name
+            );
+
+            this.messageContext.drawImage(
+                await this.getImage(`./items/messages_stat/number${userNumber++}.png`),
+                moreCenter - 64,
+                640
+            );
+
+            this.messageContext.drawImage(
+                await this.getImage('./items/messages_stat/user_circle.png'),
+                moreCenter - 28 * 2 + 1,
+                657 - 5
+            );
+
+            this.messageContext.font = `20px ${this.fontFamilyBold}`;
+            this.messageContext.fillText(user.name, moreCenter, 800);
+
+            this.messageContext.globalAlpha = 0.5;
+            this.messageContext.font = `18px ${this.fontFamilyRegular}`;
+            this.messageContext.fillText(user.description, moreCenter, 822);
+            this.messageContext.globalAlpha = 1;
+
+            moreCenter += 213;
+        }
+    }
+
+    private async drawTopUsers(x: number, y: number, users: IMessagesStatImagesUserOption[]) {
+        let xOffset = 0;
+        this.messageContext.textAlign = 'center';
+
+        for (const user of users.slice(0, 3)) {
+            await this.drawAvatar(this.messageContext, user.avatar || null, 40, x + xOffset, y, user.name);
+
+            this.messageContext.drawImage(
+                await this.getImage('./items/messages_stat/top3_circle.png'),
+                x + xOffset - 5,
+                y - 5
+            );
+            this.messageContext.font = `27px ${this.fontFamilyBold}`;
+            this.messageContext.fillText(user.name, x + xOffset + 82, y + 220);
+
+            this.messageContext.globalAlpha = 0.5;
+            this.messageContext.font = `25px ${this.fontFamilyRegular}`;
+            this.messageContext.fillText(user.description, x + xOffset + 82, y + 254);
+            this.messageContext.globalAlpha = 1;
+
+            xOffset += 272;
+        }
+
+        this.messageContext.drawImage(await this.getImage('./items/messages_stat/top3_background.png'), x + 82, y - 66);
     }
 }
