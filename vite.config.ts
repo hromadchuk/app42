9c9b454355072e7c33f88bee6bbfe3bbc637b54f
Pavlo Hromadchuk <gromadchuk.pasha@gmail.com>
2024-06-15 09:00:43 +0800

    Fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import legacy from '@vitejs/plugin-legacy';
 import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
@@ -7,8 +8,14 @@ export default defineConfig({
     base: './',
     plugins: [
         react(),
+        legacy(),
         visualizer({
             filename: 'bundle-visualizer.html'
         })
-    ]
+    ],
+    build: {
+        target: 'esnext',
+        outDir: 'dist',
+        minify: 'terser'
+    }
 });
