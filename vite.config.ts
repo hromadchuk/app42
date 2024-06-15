e9b92705a5eb8bd533b57dea2847026a35b59f37
Pavlo Hromadchuk <gromadchuk.pasha@gmail.com>
2024-06-15 09:03:41 +0800

    Revert "Fix deploy"
    This reverts commit 9c9b454355072e7c33f88bee6bbfe3bbc637b54f.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import legacy from '@vitejs/plugin-legacy';
 import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
@@ -8,14 +7,8 @@ export default defineConfig({
     base: './',
     plugins: [
         react(),
-        legacy(),
         visualizer({
             filename: 'bundle-visualizer.html'
         })
-    ],
-    build: {
-        target: 'esnext',
-        outDir: 'dist',
-        minify: 'terser'
-    }
+    ]
 });
